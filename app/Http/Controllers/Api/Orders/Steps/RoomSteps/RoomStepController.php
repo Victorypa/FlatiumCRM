85be64346d0bee9b7a7ae7ca671d4952801c3f89
LittleElijah <cuiyeqing960904@gmail.com>
2018-12-04 11:32:24 +0300

    step controllers

---
+++ app/Http/Controllers/Api/Orders/Steps/RoomSteps/RoomStepController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Orders\Steps\RoomSteps;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RoomStepController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Api/Orders/Steps/RoomSteps/Services/RoomStepServiceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Orders\Steps\RoomSteps\Services;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RoomStepServiceController extends Controller
+{
+    //
+}
