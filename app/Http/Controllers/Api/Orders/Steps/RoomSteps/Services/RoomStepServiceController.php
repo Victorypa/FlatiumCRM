739d299f9e33c035e799b961e4d175ad49d9ad8a
LittleElijah <cuiyeqing960904@gmail.com>
2018-12-10 10:15:47 +0300

    saving room_step_service pivot table

--- app/Http/Controllers/Api/Orders/Steps/RoomSteps/Services/RoomStepServiceController.php
+++ app/Http/Controllers/Api/Orders/Steps/RoomSteps/Services/RoomStepServiceController.php
@@ -17,13 +17,20 @@ class RoomStepServiceController extends Controller
 
     public function store(Order $order, OrderStep $order_step, Request $request)
     {
-        $service_ids = [];
-
         foreach ($request->selected_service_ids as $room_id => $services) {
             $room_step = $order_step->room_steps()->where('room_id', $room_id)->first();
-            dump($services);
+            $service_ids = [];
+            $service_quantities = [];
             foreach ($services as $service) {
-                $room_step->services()->sync($service['service_id']);
+                array_push($service_ids, $service['service_id']);
+                $service_quantities[$service['service_id']] = $service['quantity'];
+            }
+            $room_step->services()->sync($service_ids);
+
+            foreach ($service_quantities as $service_id => $quantity) {
+                $room_step->services()->updateExistingPivot($service_id, [
+                    'quantity' => $quantity
+                ]);
             }
         }
     }
