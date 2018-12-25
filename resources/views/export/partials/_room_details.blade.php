f2a282f616cde7e1923f66d9624183159062c9bd
LittleElijah <cuiyeqing960904@gmail.com>
2018-12-25 15:07:33 +0300

    remove zero in pdf exporting

--- resources/views/export/partials/_room_details.blade.php
+++ resources/views/export/partials/_room_details.blade.php
@@ -1,6 +1,17 @@
 <div class="room-features inline-block">
-    <div class="info-item">S: {{ (float) $room->area }} м<sup>2</sup></div>
-    <div class="info-item">H: {{ (float) $room->height }} м</div>
-    <div class="info-item">S стен: {{ (float) $room->wall_area }} м<sup>2</sup></div>
-    <div class="info-item">P: {{ (float) $room->perimeter }} м.п.</div>
+    @if ($room->area != 0)
+        <div class="info-item">S: {{ (float) $room->area }} м<sup>2</sup></div>
+    @endif
+
+    @if ($room->height != 0)
+        <div class="info-item">H: {{ (float) $room->height }} м</div>
+    @endif
+
+    @if ($room->wall_area != 0)
+        <div class="info-item">S стен: {{ (float) $room->wall_area }} м<sup>2</sup></div>
+    @endif
+
+    @if ($room->perimeter != 0)
+        <div class="info-item">P: {{ (float) $room->perimeter }} м.п.</div>
+    @endif
 </div>
