73d873bfcaa9e5b2e4a9fd63f55887363142269f
ElijahCoding <cuiyeqing960904@gmail.com>
2019-01-09 09:15:53 +0300

    dropzone with vue set up

--- resources/assets/js/components/Orders/OrderUpload.vue
+++ resources/assets/js/components/Orders/OrderUpload.vue
@@ -7,9 +7,7 @@
           <div class="col-md-10">
 
             <div class="col-md-12 px-0">
-                <button type="button" class="primary-button w-100 mp-10">
-                    Загрузить
-                </button>
+                <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions" class="mp-10"></vue-dropzone>
               <table class="table mp-10">
                 <thead>
                   <tr>
@@ -35,8 +33,24 @@
 </template>
 
 <script>
-    export default {
+    import vue2Dropzone from 'vue2-dropzone'
+    import 'vue2-dropzone/dist/vue2Dropzone.min.css'
 
+    export default {
+        data () {
+            return {
+                dropzoneOptions: {
+                    url: 'https://httpbin.org/post',
+                    thumbnailWidth: 150,
+                    maxFilesize: 0.5,
+                    headers: { "My-Awesome-Header": "header value" }
+                }
+            }
+        },
+
+        components: {
+            vueDropzone: vue2Dropzone
+        },
     }
 </script>
 
