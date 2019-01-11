32353cf169923d36853b8343bb16199a70028d28
ElijahCoding <cuiyeqing960904@gmail.com>
2019-01-11 15:07:01 +0300

    auto clear site data

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta http-equiv="Pragma" content="no-cache" />
+        <meta http-equiv="Expires" content="0" />
         <title>Flatium</title>
         <script src="{{ URL::asset('js/app.js') }}" defer></script>
         <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
