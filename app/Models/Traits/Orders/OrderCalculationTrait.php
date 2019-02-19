e134bb1994118dc451c9411056cb0b4b126f2607
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-19 08:03:00 +0300

    caculate discount and markup

--- app/Models/Traits/Orders/OrderCalculationTrait.php
+++ app/Models/Traits/Orders/OrderCalculationTrait.php
@@ -13,41 +13,31 @@ trait OrderCalculationTrait
 
         foreach ($this->rooms()->get() as $room) {
             foreach ($room->room_services()->get() as $room_service) {
+                if (!$room_service->markup) {
 
-                if ($this->discount) {
-                    if ($room_service->service->can_be_deleted) {
-                        $room_price += $room_service->service->price * (1 - (float) $this->discount / 100) * (float) $room_service->quantity;
-                    } else {
-                        $room_price += $room_service->service->price * (float) $room_service->quantity;
+                    if ($this->discount) {
+                        if ($room_service->service->can_be_discounted) {
+                            $room_service->update([
+                                'price' => $room_service->original_price * (1 - (float) $this->discount / 100)
+                            ]);
+                        }
+                    }
+
+                    if ($this->markup) {
+                        $room_service->update([
+                            'price' => $room_service->original_price * (1 + (float) $this->markup / 100)
+                        ]);
+                    }
+
+                    if (!$this->markup && !$this->discount) {
+                        $room_service->update([
+                            'price' => $room_service->original_price
+                        ]);
                     }
                 }
-
-                if ($this->markup !== null) {
-                    $room_price += $room_service->service->price * (1 + (float) $this->markup / 100) * (float) $room_service->quantity;
-                }
-
-                if ($this->markup === null && $this->discount === null) {
-                    $room_price += $room_service->service->price * (float) $room_service->quantity;
-                }
             }
-
-            $room->update([
-                'price' => (float) $room_price
-            ]);
+            $room->calculateRoomPrice($room);
         }
-
-        $total_room_price = 0;
-        $original_room_price = 0;
-
-        foreach ($this->rooms()->get() as $room) {
-            $total_room_price += (float) $room->price;
-            $original_room_price += (float) $room->original_price;
-        }
-
-        $this->update([
-            'price' => $total_room_price,
-            'original_price' => $original_room_price
-        ]);
 
     }
 }
