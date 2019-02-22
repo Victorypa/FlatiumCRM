b19a8e0e5892f3941608dea2f82c4ef619803e9a
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-22 13:02:48 +0300

    https requests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->app['request']->server->set('HTTPS', true);
+        
         Schema::defaultStringLength(191);
 
         Sheet::macro('styleCells', function (Sheet $sheet, string $cellRange, array $style) {
