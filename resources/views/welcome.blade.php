6d15eb48699f0e6f87eee3df579304a3e8569366
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-25 14:35:07 +0300

    https

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,13 @@
         <meta http-equiv="Expires" content="0" />
         <title>Flatium</title>
 
-        {{-- <script src="{{ secure_asset('js/app.js') }}" defer></script>
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
         <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ secure_asset('css/libs.min.css') }}"> --}}
+        <link rel="stylesheet" href="{{ secure_asset('css/libs.min.css') }}">
 
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        {{-- <script src="{{ asset('js/app.js') }}" defer></script>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/libs.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/libs.min.css') }}"> --}}
     </head>
     <body>
         <div id="app">
